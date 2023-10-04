2d194f8403b2318435261fc1c2d3e30292e80892
wanderwisecoder <thissurajneversets@gmail.com>
2023-10-05 00:21:19 +0530

    Updating state from a different component in Redux

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Navbar = () => {
+	const amount = useSelector((state) => state.amount);
 	return (
 		<div>
 			<nav className='navbar navbar-expand-lg bg-body-tertiary'>
@@ -26,7 +28,7 @@ const Navbar = () => {
 						</ul>
 						<div>
 							<button disable={true} className='btn btn-primary'>
-								Your Balance: 0
+								Your Balance: {amount}
 							</button>
 						</div>
 					</div>
--- src/state/action_creators/index.js
+++ src/state/action_creators/index.js
@@ -9,7 +9,7 @@ export const depositMoney = (amount) => {
 export const withdrawMoney = (amount) => {
 	return (dispatch) => {
 		dispatch({
-			type: 'deposit',
+			type: 'withdraw',
 			payload: amount,
 		});
 	};
